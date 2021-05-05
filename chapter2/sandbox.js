9a554a98f4c38f6eee3f1a89e8b004358c9e7d84
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 10:34:12 +0200

    Arrays

---
+++ chapter2/sandbox.js
@@ -0,0 +1,24 @@
+let names = ['marijan', 'kira', 'evam', 'mia']
+
+// names[1] = 'ken'
+// console.log(names)
+
+// let ages = [20, 25, 30, 35]
+
+// let random = ['maki', 30, 'kira', 5]
+// console.log(random)
+
+
+// console.log(names.length)
+
+// array methods
+
+// let res = names.join(' @')
+// let res = names.indexOf('kira')
+
+// let res = names.concat(['mia', 'eva'])
+
+// let res = names.push('new value') // alters original value
+// let res = names.pop() // alters original value and return removed value
+
+console.log(names, res)
