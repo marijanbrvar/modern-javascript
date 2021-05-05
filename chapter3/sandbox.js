47ce8c109359c5a4d9af16c27308508d8831a423
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 11:27:43 +0200

    For loop

---
+++ chapter3/sandbox.js
@@ -0,0 +1,9 @@
+// for loops
+let аrray = ['marijan', 'kira', 'mia', 'eva']
+
+for (let index = 0; index < аrray.length; index++) {
+    // const element = аrray[index];
+    let html = `<div>${аrray[index]}</div>`
+    
+    console.log(html)
+}
