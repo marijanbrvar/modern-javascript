35dab052ac67490d523e68f303bb3ec457365f16
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 11:38:40 +0200

    If Statement

---
+++ chapter3/sandbox.js
@@ -0,0 +1,17 @@
+// if statent
+const age = 20
+if (age > 20) {
+    console.log('You are over 20 years old!')
+}
+
+const names = ['maki', 'eva', 'mia', 'kira']
+
+if (names.length > 3) {
+    console.log("That's a lot of names")
+}
+
+const password = 'password'
+
+if (password.length >= 8) {
+    console.log('this password is long enought!')
+}
