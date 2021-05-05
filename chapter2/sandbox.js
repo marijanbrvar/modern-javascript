8ba4b09692169cbd596e3bf04fa7ad8aed674440
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 10:45:05 +0200

    Comparion

---
+++ chapter2/sandbox.js
@@ -0,0 +1,23 @@
+// booleans & vomparisons
+console.log(true, false)
+
+// method can return booleans
+let email = 'hello@marijan.xyz'
+let names = ['marijan', 'kira', 'mia', 'eva']
+
+// let res = email.includes('@')
+// let res = names.includes('kira')
+
+// console.log(res)
+
+// comparison operators
+
+let age = 25
+
+console.log(age === 25)
+console.log(age === 35)
+console.log(age !== 35)
+console.log(age > 35)
+console.log(age < 35)
+console.log(age <= 35)
+console.log(age >= 35)
