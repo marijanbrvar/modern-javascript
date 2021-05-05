9c75604a684365567c7bc5593246bf8b93e77fa8
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 13:54:10 +0200

    Creating object literal

---
+++ chapter5/sandbox.js
@@ -0,0 +1,11 @@
+let user = {
+    name: 'Marijan',
+    age: 49,
+    email: 'hello@marijan.xyz',
+    location: 'Belgrade',
+    blogs: ['why mac & cheese rules', '10 things to make with marmate']
+}
+
+console.log(user)
+console.log(user.blogs)
+console.log(user.name)
