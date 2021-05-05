bcf1fab059c0a0b051edd321bba5083df0526385
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 10:07:02 +0200

    Common string methods

---
+++ chapter2/sandbox.js
@@ -0,0 +1,16 @@
+// common string methods
+
+let email = 'hello@marijan.xyz'
+
+// let res = email.lastIndexOf('@')
+
+// let res = email.slice(0, 4)
+
+// let res = email.substr(5, 14)
+
+// let res = email.replace('m', 'w')
+
+// let res  = email.replace('l', 'w')
+
+
+console.log(res)
