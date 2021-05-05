3da7d126c52c149203485de5680c998cd2656f2d
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 13:07:39 +0200

    Functions vs Methods

---
+++ chapter4/sandbox.js
@@ -0,0 +1,14 @@
+const name = 'Marijan'
+
+
+// functions
+
+const greet = () => 'Hello'
+
+let resOne = greet()
+console.log(resOne)
+
+// methods
+
+let resTwo = name.toUpperCase()
+console.log(resTwo)
