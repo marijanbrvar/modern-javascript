45dc66f37a4955d9db00867f51592716274ea7a6
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 13:17:09 +0200

    Foreach Methid and Callbacks

---
+++ chapter4/sandbox.js
@@ -0,0 +1,12 @@
+// callbacks & foreach
+
+// const myFunc = callbackFunc => {
+//     let value = 50
+//     callbackFunc(value)
+// }
+
+// myFunc(value => console.log(value))
+
+let people = ['maki', 'kira', 'mia', 'eva']
+
+people.forEach((item, index) => console.log(item.toUpperCase(), index))
