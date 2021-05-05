fa2eb3681f15abcd0c067a8e18d86c9919ec9291
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 10:01:13 +0200

    Strings

---
+++ chapter2/sandbox.js
@@ -0,0 +1,26 @@
+// strings
+console.log('Hello, world!')
+
+let email = "hello@marijan.xyz"
+console.log(email)
+
+// string concatenation
+let firstName = 'Marijan';
+let lastName = 'Brvar';
+
+let fullName = firstName + ' ' + lastName
+console.log(fullName)
+
+// getting characters
+console.log(fullName[0])
+
+// string length
+console.log(fullName.length)
+
+// string methods
+console.log(fullName.toUpperCase())
+let res = fullName.toLowerCase()
+console.log(res, fullName)
+
+let index = email.indexOf('@')
+console.log(index)
