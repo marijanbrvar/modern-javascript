3a9974dada346555afc32811ad4ab2c4673a132d
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 10:18:06 +0200

    Numbers

---
+++ chapter2/sandbox.js
@@ -0,0 +1,39 @@
+// number
+
+let radius = 10
+const pi = 3.14
+
+// console.log(radius, pi)
+
+
+// Math operators +, -, *, /, **, %
+
+// console.log(10 / 2)
+// let res = radius % 3
+
+// let res = pi * radius**2
+
+// order of operation - BITMAS
+
+// let res = 5 * (10 -3)**2
+// 5 * 7 ** 2
+// 5 * 49
+// 245
+
+// console.log(res)
+
+let likes = 10
+// likes = likes + 1
+// likes++
+// likes += 10
+// likes *= 2
+
+// console.log(likes)
+
+// NaN — not a number
+
+// console.log(5 / 'Number')
+// console.log(5 * 'Number')
+
+let res = 'the blog has ' + likes + ' likes'
+console.log(res)
