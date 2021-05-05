5bb346b6b2a709b4e55f77a1e54e6f861d29d79b
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 10:54:16 +0200

    Type conversion

---
+++ chapter2/sandbox.js
@@ -0,0 +1,16 @@
+let score = '100'
+
+// score = Number(score)
+// console.log(score + 1)
+
+// console.log(typeof score)
+
+
+// let res = '50'
+// res = String(res)
+
+// let res = Boolean(100)
+// let res = Boolean(0)
+let res = Boolean('0')
+
+console.log(typeof res, res)
