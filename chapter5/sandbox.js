0838c5b5f5bfbce2d39e07ef56168922874786d9
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 17:39:43 +0200

    Creating object literal

--- chapter5/sandbox.js
+++ chapter5/sandbox.js
@@ -1,3 +1,4 @@
+// Creating object literal
 let user = {
     name: 'Marijan',
     age: 49,
@@ -8,4 +9,13 @@ let user = {
 
 console.log(user)
 console.log(user.blogs)
+// console.log(user.name) // accesing value
+const key = 'location'
+
+console.log(user[key]) // accesing key
+
+user['name'] = 'makiato'
 console.log(user.name)
+
+
+console.log(typeof user);
