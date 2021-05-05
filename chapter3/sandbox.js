e876bfaedcdfb8c7c1f6dfae155b4d5df25b3563
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 12:08:00 +0200

    Switch Statement

---
+++ chapter3/sandbox.js
@@ -0,0 +1,37 @@
+// switch statement
+
+const grade = 'D'
+
+// if (grade == 'A') {
+    
+// } else if (grade == 'B') {
+    
+// } else if (grade == 'C') {
+    
+// } else if (grade == 'D') {
+    
+// } else if (grade == 'E') {
+    
+// } else {
+    
+// }
+
+switch(grade) {
+    case 'A':
+        console.log('you got and A');
+        break
+    case 'B':
+        console.log('you got and B');
+        break
+    case 'C':
+        console.log('you got and C');
+        break
+    case 'D':
+        console.log('you got and D');
+        break
+    case 'E':
+        console.log('you got and E');
+        break
+    default:
+        console.log('not a valid grade!');
+}
