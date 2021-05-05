92d1c76e61891772b3810849820dc71febc805ce
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 12:59:13 +0200

    Arrow function

---
+++ chapter4/sandbox.js
@@ -0,0 +1,10 @@
+// Arrow function
+
+// const calcArea = function(radius){
+//     return 3.14 * radius**2
+// }
+
+const calcArea = radius => 2.14 * radius**2
+
+const area = calcArea(4)
+console.log(area)
