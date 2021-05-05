2542680a845cc3feed9b81db5e9c705257e999d0
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-05 13:02:50 +0200

    Arrow function

--- chapter4/sandbox.js
+++ chapter4/sandbox.js
@@ -8,3 +8,14 @@ const calcArea = radius => 2.14 * radius**2
 
 const area = calcArea(4)
 console.log(area)
+
+
+const bill = (products, tax) => {
+    let total = 0;
+    for (let index = 0; index < products.length; index++) {
+       total += products[index] + products[index] * tax
+    }
+    return total;
+}
+
+console.log(bill([10,15,30, 20], 0.2))
