115f65849f15d9b6b2d791dbbfe05044852d1696
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-10 20:44:29 +0200

    primitive & reference values

--- chapter5/sandbox.js
+++ chapter5/sandbox.js
@@ -1,26 +1,24 @@
-// object literal in array
-let user = {
-    name: 'Marijan',
-    age: 49,
-    email: 'hello@marijan.xyz',
-    location: 'Belgrade',
-    blogs: [
-        {title: 'why mac & cheese rules', like: 30}, 
-        {title: '10 things to make with marmate', like: 50}
-    ],
-    login: function(){
-        console.log('the user logged in')
-    },
-    logout() {
-        console.log('the user logged out!')
-    },
-    logBlogs(){
-        // console.log(this)
-        console.log('this user has written the following blogs: ')
-        this.blogs.forEach(blog => console.log(blog.title, blog.like))
-    }
-}
+//primitive values
 
-user.logBlogs()
+let scoreOne = 50
+let scoreTwo = scoreOne;
 
-console.log(this)
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`)
+
+// scoreOne = 100
+
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`)
+
+
+
+// reference value
+
+const userOne = { name: 'Kira', age: 30 }
+const userTwo = userOne
+
+
+console.log(userOne, userTwo)
+
+userOne.age = 40
+
+console.log(userOne, userTwo)
