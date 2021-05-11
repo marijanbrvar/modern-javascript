c25499353bce51b34d5e16ec139648d98c77f42c
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-11 11:26:49 +0200

    Creating and removing elements

--- chapter6/sandbox.js
+++ chapter6/sandbox.js
@@ -1,15 +1,20 @@
-// const button = document.querySelector('button')
-
-// button.addEventListener('click', e => {
-//     console.log(e)
-// })
-
+const button = document.querySelector('button')
 const items = document.querySelectorAll('li')
+const ul = document.querySelector('ul')
+
+button.addEventListener('click', ()=>{
+    // ul.innerHTML += `<li>add new line</li>`
+    const li = document.createElement('li')
+    li.textContent = 'Something new to add'
+    // ul.append(li)
+    ul.prepend(li)
+})
 
 items.forEach(item => {
     item.addEventListener('click', e => {
-        // console.log('item clicked', e.target)
-        // console.log(item)
-        e.target.style.textDecoration = 'line-through'
+        // e.target.style.textDecoration = 'line-through'
+        e.target.remove()
     })
 })
+
+// ul.remove()
