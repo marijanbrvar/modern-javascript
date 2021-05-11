acd8a3fb4b355fcdbd46367a9185c4c58e7e9db1
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-11 12:18:34 +0200

    DOM events

--- chapter6/sandbox.js
+++ chapter6/sandbox.js
@@ -1,28 +1,18 @@
-const button = document.querySelector('button')
-const items = document.querySelectorAll('li')
-const ul = document.querySelector('ul')
+const copy = document.querySelector('.copy-me');
 
-button.addEventListener('click', ()=>{
-    // ul.innerHTML += `<li>add new line</li>`
-    const li = document.createElement('li')
-    li.textContent = 'Something new to add'
-    // ul.append(li)
-    ul.prepend(li)
+copy.addEventListener('copy', ()=>{
+    console.log('Oi! my content is copyright')
 })
 
-// items.forEach(item => {
-//     item.addEventListener('click', e => {
-//         // e.target.style.textDecoration = 'line-through'
-//         console.log('event in li')
-//         e.stopPropagation()
-//         e.target.remove()
-//     })
-// })
 
-ul.addEventListener('click', e => {
-    // console.log('event in ul')
-    if (e.target.tagName === 'LI') {
-        e.target.remove()
-    }
+const box = document.querySelector('.box')
+
+box.addEventListener('mousemove', e => {
+    // console.log(e.offsetX, e.offsetY)
+    box.textContent = `X: ${e.offsetX} / Y: ${e.offsetY}`
 })
-// ul.remove()
+
+
+document.addEventListener('wheel', e => {
+    console.log(e.pageX, e.pageY)
+})
